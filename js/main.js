7b6536d05e877f2762e44825fbbf8058936cfc82
MiguelAngel <MiguelAngel@Kanyu-portatil>
2015-06-03 14:09:49 +0200

    Interfaz principal subida

---
+++ js/main.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    var mobile = document.createElement('div');
+    mobile.className = 'nav-mobile';
+    document.querySelector('nav').appendChild(mobile);
+    
+    
+    var mobileNav = document.querySelector('.nav-mobile');
+    mobileNav.onclick = function() {
+      toggleMenu();
+        
+    }
+    
+    var notificaciones = document.getElementById("notificaciones-toggle");
+    var toggle2 = document.querySelector('.notificaciones-list');
+    notificaciones.onclick = function(){
+        toggleNotificaciones();
+    }
+
+        // hasClass
+    function hasClass(elem, className) {
+      return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    }
+
+    // toggleClass
+    function toggleMenu() {
+      var menu = document.getElementById("manageinfo");
+      var notificacionesm = document.getElementById("notificaciones-list");
+        if (menu.style.display =="none") {
+            menu.style.display = "flex";
+        } else {
+            menu.style.display = "none";
+            if(notificacionesm.style.display == "block")
+                notificacionesm.style.display == "none";
+        }
+    }
+    
+    function toggleNotificaciones() {
+      var notificacionesm = document.getElementById("notificaciones-list");
+        if (notificacionesm.style.display =="none") {
+            notificacionesm.style.display = "block";
+        } else {
+            notificacionesm.style.display = "none";
+        }
+    }
+};
