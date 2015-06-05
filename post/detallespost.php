aae62f01871afd296e3a21cc87e03166d7870615
MiguelAngel <MiguelAngel@Kanyu-portatil>
2015-06-05 13:18:36 +0200

    Me gustan y post detallado con estilos completos

--- post/detallespost.php
+++ post/detallespost.php
@@ -1,17 +1,17 @@
 <?php
-    require '../require/comun.php';
-    $sesion->autentificado("../index.php");
-    $bd = new BaseDatos();
-    $id = Leer::get("id");
-    $modeloPost = new ModeloPost($bd);
-    $post = $modeloPost->get($id);
-    $modeloUsuario = new ModeloUsuario($bd);
-    $usuario = $modeloUsuario->get($post->getLogin());
-    $login = $sesion->getUsuario()->getLogin();
-    $megusta = new Megusta($login, $id);
-    $modelomegusta = new ModeloMegusta($bd);
-    $countmegusta = $modelomegusta->count($megusta);               
-    $idelemento = "seguir0";
+require '../require/comun.php';
+$sesion->autentificado("../index.php");
+$bd = new BaseDatos();
+$id = Leer::get("id");
+$modeloPost = new ModeloPost($bd);
+$post = $modeloPost->get($id);
+$modeloUsuario = new ModeloUsuario($bd);
+$usuario = $modeloUsuario->get($post->getLogin());
+$login = $sesion->getUsuario()->getLogin();
+$megusta = new Megusta($login, $id);
+$modelomegusta = new ModeloMegusta($bd);
+$countmegusta = $modelomegusta->count($megusta);
+$idelemento = "seguir0";
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,39 +19,102 @@
         <meta charset="UTF-8">
         <title>Blackboard</title>
         <script src="../js/js.js"></script>
+        <script src="../js/main2.js"></script>
+        <link rel="stylesheet" type="text/css" href="../styles/styles2.css">
     </head>
     <body>
-        <nav>
-            <ul>
-                <li><a href="../index.php">Inicio</a></li>
-                <li><a href="megusta.php">Me Gusta</a></li>
-                <li><a href="mispost.php">Mis post</a></li>
-                <li><a href="../usuario/viewperfil.php">Panel de administración</a></li>
-                <li><a href="../usuario/phpcerrarsesion.php">Desloguear</a></li>
-                <li>
-                    <form action="javascript:enviar(this.buscar)" name="buscar" method="post">
-                        <input type="text" placeholder="Buscar" name="texto">
+        <header>
+            <div class="div-logo">
+                <a href="../index.php"><img src="../img/logoprincipalm.png"/></a>
+                <div class="links">
+                    <div><a href="megusta.php"><span>h</span>Me gustan</a></div>
+                    <div><a href="mispost.php"><span>Y</span>Mis posts</a></div>
+                </div>
+            </div>
+            <nav>
+                <div class="search">
+                    <div class="userpanel" id="primary_nav_wrap">
+                        <ul>
+                            <li>
+                                <a href="#"><span>A</span></a>
+                                <ul>
+                                    <li><a href="../usuario/viewperfil.php"><span>C</span>Panel de control</a></li>
+                                    <li><a href="../usuario/phpcerrarsesion.php"><span>e</span>Cerrar sesión</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <form id="form-buscar" action="javascript:enviar(this.buscar)" name="buscar" method="post">
+                        <input type="search" placeholder="Buscar" id="busqueda" name="texto">
                     </form>
-                </li>
-            </ul>
-        </nav>
-        <a href="verusuario.php?login=<?php echo $post->getLogin();?>"><?php echo $usuario->getNombre()." ".$usuario->getApellidos(); ?></a><br>
-        <div>
-            <?php echo $post->getFechapost(); ?><br>
-            <?php echo $post->getDescripcion(); ?><br>
-            <?php echo $post->getGusta(); ?>
-            <?php if($countmegusta>0){ ?>
-                <a style="color:blue;" id="<?php echo $idelemento; ?>" href="javascript:gusta('<?php echo $idelemento; ?>','<?php echo $login; ?>','<?php echo $id; ?>')">No me gusta</a>
-            <?php }else{ ?>
-                <a style="color:blue;" id="<?php echo $idelemento; ?>" href="javascript:gusta('<?php echo $idelemento; ?>','<?php echo $login; ?>','<?php echo $id; ?>')">Me gusta</a>
-            <?php } ?>
-        </div>
-        <?php             
-            $modeloArchivo = new ModeloArchivospost($bd);
-            $archivos = $modeloArchivo->getListTotal($id);
-        ?>
-        <?php foreach ($archivos as $key => $archivo) { ?>
-            <a style="color:red;" target="_blank" href="../archivos/<?php echo $archivo->getUrl(); ?>">archivico</a>
-        <?php } $bd->closeConsulta(); ?>
+                </div>
+            </nav>
+        </header>
+        <div class="contenedor-main">
+            <aside id="manageinfo2" class="manageinfo2">
+                <div class="bloque">
+                    <div class="userinfo">
+                        <div class="linksm">
+                            <div><a href="megusta.php"><span>h</span>Me gustan</a></div>
+                            <div><a href="mispost.php"><span>Y</span>Mis posts</a></div>
+                        </div>
+                    </div>
+                    <div class="searchm">
+                        <div class="userpanel" id="primary_nav_wrap">
+                            <ul>
+                                <li>
+                                    <a href="#"><span>A</span></a>
+                                    <ul>
+                                        <li><a href="../usuario/viewperfil.php"><span>C</span>Panel de control</a></li>
+                                        <li><a href="../usuario/phpcerrarsesion.php"><span>e</span>Cerrar sesión</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <form id="form-buscar" action="javascript:enviar2(this.buscar2)" name="buscar2" method="post">
+                            <input type="search" placeholder="Buscar" id="busqueda" name="texto">
+                        </form>
+                    </div>
+                </div>
+            </aside>
+            <section class="siguiendo2" id="lista_posts">
+                <div clas="post">
+                    <div class="bloq-sup-post">
+                        <div class="div-foto">
+                            <img src="../archivos/<?php echo $usuario->getUrlfoto(); ?>">
+                        </div>
+                        <div class="nombre-poster">
+                            <a href="verusuario.php?login=<?php echo $post->getLogin(); ?>"><?php echo $usuario->getNombre() . " " . $usuario->getApellidos(); ?></a>
+                        </div>
+                    </div>
+                    <div class="post-body">
+                        <div class="fecha-post">
+                            <?php echo $post->getFechapost(); ?>
+                        </div>
+                        <div class="mensaje-post">
+                            <?php echo $post->getDescripcion(); ?>
+                        </div>
+                        <div class="div-megusta">
+                            Me gusta: <?php echo $post->getGusta(); ?>
+                        </div>
+                        <div>
+                            <?php if ($countmegusta > 0) { ?>
+                                <a style="color:blue;" id="<?php echo $idelemento; ?>" href="javascript:gusta('<?php echo $idelemento; ?>','<?php echo $login; ?>','<?php echo $id; ?>')">No me gusta</a>
+                            <?php } else { ?>
+                                <a style="color:blue;" id="<?php echo $idelemento; ?>" href="javascript:gusta('<?php echo $idelemento; ?>','<?php echo $login; ?>','<?php echo $id; ?>')">Me gusta</a>
+                            <?php } ?>
+                        </div>
+                    </div>
+                    <div class="archivos-box">
+                        <?php
+                        $modeloArchivo = new ModeloArchivospost($bd);
+                        $archivos = $modeloArchivo->getListTotal($id);
+                        ?>
+                        <?php foreach ($archivos as $key => $archivo) { ?>
+                            <a style="color:red;" target="_blank" href="../archivos/<?php echo $archivo->getUrl(); ?>">archivico</a>
+                        <?php } $bd->closeConsulta(); ?>
+                    </div>
+                </div>
+            </section>
     </body>
 </html>
--- post/megusta.php
+++ post/megusta.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <?php
-    require '../require/comun.php';
-    $sesion->autentificado("../index.php");
-    unset($_SESSION["cantidadcargadas"]);
-    unset($_SESSION["contador"]);
-    $bd = new BaseDatos();
-    $modeloPost = new ModeloPost($bd);
-    $usuario = $sesion->getUsuario();
-    $login = $usuario->getLogin();
-    $posts=$modeloPost->postMeGustan(0, 10, $login);    
+require '../require/comun.php';
+$sesion->autentificado("../index.php");
+unset($_SESSION["cantidadcargadas"]);
+unset($_SESSION["contador"]);
+$bd = new BaseDatos();
+$modeloPost = new ModeloPost($bd);
+$usuario = $sesion->getUsuario();
+$login = $usuario->getLogin();
+$posts = $modeloPost->postMeGustan(0, 10, $login);
 ?>
 
 <html>
@@ -17,61 +17,126 @@
         <title>Blackboard</title>
         <script type="text/javascript" src="../js/js.js"></script>
         <script type="text/javascript" src="../js/javascriptscroll.js"></script>
+        <script src="../js/main2.js"></script>
+        <link rel="stylesheet" type="text/css" href="../styles/styles2.css">
     </head>
     <body>
-        <nav>
-            <ul>
-                <li><a href="../index.php">Inicio</a></li>
-                <li><a href="megusta.php">Me Gusta</a></li>
-                <li><a href="mispost.php">Mis post</a></li>
-                <li><a href="../usuario/viewperfil.php">Panel de administración</a></li>
-                <li><a href="../usuario/phpcerrarsesion.php">Desloguear</a></li>
-                <li>
-                    <form action="javascript:enviar(this.buscar)" name="buscar" method="post">
-                        <input type="text" placeholder="Buscar" name="texto">
-                    </form>
-                </li>
-            </ul>
-        </nav>
-        <div id="lista_posts">
-        <?php 
-        if(count($posts) == 0) echo "No hay posts para mostrar"; 
-        $contador = 0;
-        foreach ($posts as $key => $post) {
-            $idpost = $post->getIdpost();
-            $modeloArchivo = new ModeloArchivospost($bd);
-            $archivos = $modeloArchivo->getListTotal($idpost);
-            $megusta = new Megusta($login, $idpost);
-            $modelomegusta = new ModeloMegusta($bd);
-            $countmegusta = $modelomegusta->count($megusta); 
-            $idelemento = "gusta".$contador;
-            $modelousuariopost = new ModeloUsuario($bd);
-            $usuariopost = $modelousuariopost->get($post->getLogin());
-        ?>
-        <div>
-            <img width="30" src="../archivos/<?php echo $usuariopost->getUrlfoto(); ?>">
-            <a href="verusuario.php?login=<?php echo $post->getLogin(); ?>"><?php echo $usuariopost->getNombre()." ".$usuariopost->getApellidos(); ?></a>
-            <a href="detallespost.php?id=<?php echo $post->getIdpost(); ?>"><?php echo $post->getFechapost(); ?></a><br>
-            <?php echo $post->getDescripcion(); ?><br>
-            <?php echo $post->getGusta(); ?>
-            <?php if($countmegusta>0){ ?>
-                <a style="color:blue;" id="<?php echo $idelemento; ?>" href="javascript:gusta('<?php echo $idelemento; ?>','<?php echo $login; ?>','<?php echo $idpost; ?>')">No me gusta</a>
-            <?php }else{ ?>
-                <a style="color:blue;" id="<?php echo $idelemento; ?>" href="javascript:gusta('<?php echo $idelemento; ?>','<?php echo $login; ?>','<?php echo $idpost; ?>')">Me gusta</a>
-            <?php } ?>
-        </div>
-        <?php foreach ($archivos as $key => $archivo) { ?>
-            <a style="color:red;" target="_blank" href="../archivos/<?php echo $archivo->getUrl(); ?>">archivico</a>
-        <?php } ?>
-        <?php
-        $contador++;
-        }if(count($posts) > 0){ 
-        ?>
-            <br>
-            <input type="button" id="mas" onclick="javascript:cargarMeGusta('<?php echo $login; ?>');" value="Cargar más">
-        <?php }
-        $bd->closeConsulta(); ?>
+        <header>
+            <div class="div-logo">
+                <a href="../index.php"><img src="../img/logoprincipalm.png"/></a>
+                <div class="links">
+                    <div><a href="megusta.php"><span>h</span>Me gustan</a></div>
+                    <div><a href="mispost.php"><span>Y</span>Mis posts</a></div>
+                </div>
             </div>
+            <nav>
+                <div class="search">
+                    <div class="userpanel" id="primary_nav_wrap">
+                        <ul>
+                            <li>
+                                <a href="#"><span>A</span></a>
+                                <ul>
+                                    <li><a href="../usuario/viewperfil.php"><span>C</span>Panel de control</a></li>
+                                    <li><a href="../usuario/phpcerrarsesion.php"><span>e</span>Cerrar sesión</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <form id="form-buscar" action="javascript:enviar(this.buscar)" name="buscar" method="post">
+                        <input type="search" placeholder="Buscar" id="busqueda" name="texto">
+                    </form>
+                </div>
+            </nav>
+        </header>
+        <div class="contenedor-main">
+            <aside id="manageinfo2" class="manageinfo2">
+                <div class="bloque">
+                    <div class="userinfo">
+                        <div class="linksm">
+                            <div><a href="megusta.php"><span>h</span>Me gustan</a></div>
+                            <div><a href="mispost.php"><span>Y</span>Mis posts</a></div>
+                        </div>
+                    </div>
+                    <div class="searchm">
+                        <div class="userpanel" id="primary_nav_wrap">
+                            <ul>
+                                <li>
+                                    <a href="#"><span>A</span></a>
+                                    <ul>
+                                        <li><a href="../usuario/viewperfil.php"><span>C</span>Panel de control</a></li>
+                                        <li><a href="../usuario/phpcerrarsesion.php"><span>e</span>Cerrar sesión</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <form id="form-buscar" action="javascript:enviar2(this.buscar2)" name="buscar2" method="post">
+                            <input type="search" placeholder="Buscar" id="busqueda" name="texto">
+                        </form>
+                    </div>
+                </div>
+            </aside>
+            <section class="siguiendo2" id="lista_posts">
+                <div class="titulo-estandar">Posts que me gustan</div>
+                <?php
+                if (count($posts) == 0)
+                    echo "No hay posts para mostrar";
+                $contador = 0;
+                foreach ($posts as $key => $post) {
+                    $idpost = $post->getIdpost();
+                    $modeloArchivo = new ModeloArchivospost($bd);
+                    $archivos = $modeloArchivo->getListTotal($idpost);
+                    $megusta = new Megusta($login, $idpost);
+                    $modelomegusta = new ModeloMegusta($bd);
+                    $countmegusta = $modelomegusta->count($megusta);
+                    $idelemento = "gusta" . $contador;
+                    $modelousuariopost = new ModeloUsuario($bd);
+                    $usuariopost = $modelousuariopost->get($post->getLogin());
+                    ?>
+                    <article class="post">
+                        <div class="bloq-sup-post">
+                            <div class="div-foto">
+                                <img width="30" src="../archivos/<?php echo $usuariopost->getUrlfoto(); ?>">
+                            </div>
+                            <div class="nombre-poster">
+                                <a href="verusuario.php?login=<?php echo $post->getLogin(); ?>"><?php echo $usuariopost->getNombre() . " " . $usuariopost->getApellidos(); ?></a>
+                            </div>
+                        </div>
+                        <div class="bloq-inf-post">
+                            <div class="post-body">
+                                <div class="fecha-post">
+                                    <a href="detallespost.php?id=<?php echo $post->getIdpost(); ?>"><?php echo $post->getFechapost(); ?></a>
+                                </div>
+                                <div class="mensaje-post">
+                                    <?php echo $post->getDescripcion(); ?>
+                                </div>
+                                <div class="div-megusta">
+                                    Me gusta: <?php echo $post->getGusta(); ?>
+                                </div>
+                                <?php if ($countmegusta > 0) { ?>
+                                    <a style="color:blue;" id="<?php echo $idelemento; ?>" href="javascript:gusta('<?php echo $idelemento; ?>','<?php echo $login; ?>','<?php echo $idpost; ?>')">No me gusta</a>
+                                <?php } else { ?>
+                                    <a style="color:blue;" id="<?php echo $idelemento; ?>" href="javascript:gusta('<?php echo $idelemento; ?>','<?php echo $login; ?>','<?php echo $idpost; ?>')">Me gusta</a>
+                                <?php } ?>
+                            </div>
+                        </div>
+                        <div class="archivos-box">
+                            <?php foreach ($archivos as $key => $archivo) { ?>
+                                <a style="color:red;" target="_blank" href="../archivos/<?php echo $archivo->getUrl(); ?>">archivico</a>
+                            <?php } ?>
+                        </div>
+                        <?php
+                        $contador++;
+                    }
+                    $bd->closeConsulta();
+                    ?>
+                </article>
+                <?php if (count($posts) > 0) {
+                    ?>
+                    <div class="div-cargar">
+                        <button class="boton-cargar" id="mas" onclick="javascript:cargarMeGusta('<?php echo $login; ?>');">Cargar más</button>
+                    </div>
+                <?php } ?>
+            </section>
     </body>
 </html>
 <?php $bd->closeConexion(); ?>
